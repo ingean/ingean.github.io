5f9d2bb252fd4af1b4ffdfb7511152975200aebd
inge <inge.anundskas@geodata.no>
2019-08-22 09:41:01 +0200

    Service area

--- ruteberegning/js/routing.js
+++ ruteberegning/js/routing.js
@@ -3,57 +3,85 @@ require([
   "esri/views/MapView",
   "esri/Graphic",
   "esri/layers/GraphicsLayer",
-  "esri/tasks/RouteTask",
-  "esri/tasks/support/RouteParameters",
+  "esri/tasks/ServiceAreaTask",
+  "esri/tasks/support/ServiceAreaParameters",
   "esri/tasks/support/FeatureSet"
 ], function(
   Map,
   MapView,
   Graphic,
   GraphicsLayer,
-  RouteTask,
-  RouteParameters,
+  ServiceAreaTask,
+  ServiceAreaParameters,
   FeatureSet
 ) {
   // Point the URL to a valid route service that uses an
   // ArcGIS Online hosted service proxy
-  var routeTask = new RouteTask({
-    url:
-      "https://utility.arcgis.com/usrsvcs/appservices/srsKxBIxJZB0pTZ0/rest/services/World/Route/NAServer/Route_World"
+  
+
+  var serviceAreaTask = new ServiceAreaTask({
+    url: "https://utility.arcgis.com/usrsvcs/appservices/jbXaRK9bZe4Dm34X/rest/services/World/ServiceAreas/NAServer/ServiceArea_World/solveServiceArea"
   });
 
-  // The stops and route result will be stored in this layer
-  var routeLayer = new GraphicsLayer();
+  function createGraphic(point) {
+    // Remove any existing graphics
+    view.graphics.removeAll();
+    // Create a and add the point
+    var graphic = new Graphic({
+      geometry: point,
+      symbol: {
+        type: "simple-marker",
+        color: "white",
+        size: 8
+      }
+    });
+    view.graphics.add(graphic);
+    return graphic;
+  }
 
-  // Setup the route parameters
-  var routeParams = new RouteParameters({
-    stops: new FeatureSet(),
-    outSpatialReference: {
-      // autocasts as new SpatialReference()
-      wkid: 3857
-    }
+  
+  view.on("click", function(event){
+    var locationGraphic = createGraphic(event.mapPoint);
+    
+    var driveTimeCutoffs = [10]; // Minutes (default)
+    var serviceAreaParams = createServiceAreaParams(locationGraphic, driveTimeCutoffs, view.spatialReference);
+    executeServiceAreaTask(serviceAreaParams);
   });
 
-  // Define the symbology used to display the stops
-  var stopSymbol = {
-    type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-    style: "cross",
-    size: 15,
-    outline: {
-      // autocasts as new SimpleLineSymbol()
-      width: 4
-    }
-  };
+  function createServiceAreaParams(locationGraphic, driveTimeCutoffs, outSpatialReference) {
+    // Create one or more locations (facilities) to solve for
+    var featureSet = new FeatureSet({
+      features: [locationGraphic]
+    });
+    // Set all of the input parameters for the service
+    var taskParameters = new ServiceAreaParams({
+      facilities: featureSet, // Location(s) to start from
+      defaultBreaks: driveTimeCutoffs, // One or more drive time cutoff values
+      outSpatialReference: outSpatialReference // Spatial reference to match the view
+    });
+    return taskParameters;
+  }
 
-  // Define the symbology used to display the route
-  var routeSymbol = {
-    type: "simple-line", // autocasts as SimpleLineSymbol()
-    color: [0, 0, 255, 0.5],
-    width: 5
-  };
+  function executeServiceAreaTask(serviceAreaParams) {
+    return serviceAreaTask.solve(serviceAreaParams)
+      .then(function(result){
+        if (result.serviceAreaPolygons.length) {
+          // Draw each service area polygon
+          result.serviceAreaPolygons.forEach(function(graphic){
+            graphic.symbol = {
+              type: "simple-fill",
+              color: "rgba(255,50,50,.25)"
+            }
+            view.graphics.add(graphic,0);
+          });
+        }
+      }, function(error){
+        console.log(error);
+      });
+  }
 
   var map = new Map({
-    basemap: "streets",
+    basemap: "streets-navigation-vector",
     layers: [routeLayer] // Add the route layer to the map
   });
   var view = new MapView({
@@ -63,27 +91,4 @@ require([
     zoom: 11
   });
 
-  // Adds a graphic when the user clicks the map. If 2 or more points exist, route is solved.
-  view.on("click", addStop);
-
-  function addStop(event) {
-    // Add a point at the location of the map click
-    var stop = new Graphic({
-      geometry: event.mapPoint,
-      symbol: stopSymbol
-    });
-    routeLayer.add(stop);
-
-    // Execute the route task if 2 or more stops are input
-    routeParams.stops.features.push(stop);
-    if (routeParams.stops.features.length >= 2) {
-      routeTask.solve(routeParams).then(showRoute);
-    }
-  }
-  // Adds the solved route to the map as a graphic
-  function showRoute(data) {
-    var routeResult = data.routeResults[0].route;
-    routeResult.symbol = routeSymbol;
-    routeLayer.add(routeResult);
-  }
 });
