2e2baa769642a5fc48a7686a63f93adafe4fbaaf
inge <inge.anundskas@geodata.no>
2019-11-27 10:01:02 +0100

    https

--- evakuering/config/service_urls.js
+++ evakuering/config/service_urls.js
@@ -7,5 +7,5 @@ const clientSecret = '1243db4379f543f1b6be899fcbef8aea';
 const url_Token = 'https://www.arcgis.com/sharing/rest/oauth2/token';
 
 //GP services
-const url_evacGP = 'http://demo09.geodata.no/arcgis/rest/services/GP_Tjenester/UMS_Evakuering_GP/GPServer/UMS%20Evakuering';
+const url_evacGP = 'https://demo09.geodata.no/arcgis/rest/services/GP_Tjenester/UMS_Evakuering_GP/GPServer/UMS%20Evakuering';
 const evacIter = 10;
