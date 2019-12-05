ebddb1bd180f0979277da2c2446790db9d5e64c8
inge <inge.anundskas@geodata.no>
2019-12-05 13:51:55 +0100

    Bugfix

--- time/js/time.js
+++ time/js/time.js
@@ -1,5 +1,8 @@
 function showTime() {
   var date = new Date();
+  var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+  var locale = 'no-NB'
+
   var h = date.getHours(); // 0 - 23
   var m = date.getMinutes(); // 0 - 59
   var s = date.getSeconds(); // 0 - 59
@@ -9,13 +12,15 @@ function showTime() {
   s = (s < 10) ? "0" + s : s;
   
   var time = h + ":" + m + ":" + s;
-  var date = date.getDate() + "." + (Number(date.getMonth()) + 1) + "." + date.getFullYear();
+  //var datestring = date.getDate() + "." + (Number(date.getMonth()) + 1) + "." + date.getFullYear();
+  
+  var datestring = date.toLocaleDateString(locale, options);
 
   $('#timeDisplay').html(time);
   $('#timeDisplay').text(time);
 
-  $('#dateDisplay').html(date);
-  $('#dateDisplay').text(date);
+  $('#dateDisplay').html(datestring);
+  $('#dateDisplay').text(datestring);
   //document.getElementById("timeDisplay").innerText = time;
   //document.getElementById("timeDisplay").textContent = time;
   
