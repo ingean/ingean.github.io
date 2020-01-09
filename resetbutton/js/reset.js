7db73855c65783429d2530c235095f01e632393f
inge <inge.anundskas@geodata.no>
2020-01-09 10:11:01 +0100

    Added spinner

--- resetbutton/js/reset.js
+++ resetbutton/js/reset.js
@@ -1,6 +1,16 @@
 
 function reset() {
+  btnSpinner(true, '#btnReset');
   fetch(urlProxyReset)
   .then(console.log('New shift succeeded'))
   .catch(console.log('New shift failed'))
+  .finally(btnSpinner(false))
+}
+
+function btnSpinner(activate, btnID) {
+  if(activate === true) {
+    $(btnID).append('<span id="spinner-loading" class="spinner-border spinner-border-sm"></span>');
+  } else {
+    $('#spinner-loading').remove();
+  }
 }
